8f0342ab2d69ef4624d981d4ebda9d24005a38e2
Nikola Pavicevic <nijepa@gmail.com>
2021-04-09 12:49:00 +0200

    skip link

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,66 +1,84 @@
 <template>
   <div class="">
-    <!-- <transition name="slide" mode="out-in"> -->
+    <a class="skip-link" href="#maincontent">Skip to main</a>
+    <div class="" id="maincontent">
+      <!-- <transition name="slide" mode="out-in"> -->
       <Navigation />
-    <!-- </transition> -->
-    
-    <transition name="slide" mode="out-in">
-      <router-view />
-    </transition>
-    
-    <Footer></Footer>
+      <!-- </transition> -->
 
-    <to-top-button />
+      <transition name="slide" mode="out-in">
+        <router-view />
+      </transition>
+
+      <Footer></Footer>
+
+      <to-top-button />
+    </div>
   </div>
 </template>
 
 <script>
-  import Navigation from "@/components/Navigation.vue";
-  import Footer from "@/components/Footer.vue";
-  import ToTopButton from "@/components/utils/ToTopButton.vue";
-  import { mapActions } from 'vuex';
+import Navigation from "@/components/Navigation.vue";
+import Footer from "@/components/Footer.vue";
+import ToTopButton from "@/components/utils/ToTopButton.vue";
+import { mapActions } from "vuex";
 
-  export default {
-    name: 'Dashboard',
+export default {
+  name: "Dashboard",
 
-    components: {
-      Navigation, 
-      Footer,
-      ToTopButton
-    },
+  components: {
+    Navigation,
+    Footer,
+    ToTopButton,
+  },
 
-    methods: {
-      ...mapActions([ 'initialState' ]),
-    },
+  methods: {
+    ...mapActions(["initialState"]),
+  },
 
-    created() {
-      this.initialState();
-    } 
-  }
+  created() {
+    this.initialState();
+  },
+};
 </script>
 
 <style>
-  .slide-enter-active {
-    transition: all .5s ease-in-out;
-    /* position: absolute; */
-  }
+/* style.css */
+.skip-link {
+  position: absolute;
+  top: -40px;
+  left: 0;
+  background: #000000;
+  color: white;
+  padding: 8px;
+  z-index: 100;
+}
 
-  .slide-leave-active {
-    transition: all .5s ease-in-out;
-    /* position: absolute; */
-  }
+.skip-link:focus {
+  top: 0;
+}
 
-  .slide-enter {
-    transform: translateX(-2em);
-    opacity: 0;
-  }
+.slide-enter-active {
+  transition: all 0.5s ease-in-out;
+  /* position: absolute; */
+}
 
-  .slide-leave-to {
-    transform: translateX(2em);
-    opacity: 0;
-  }
+.slide-leave-active {
+  transition: all 0.5s ease-in-out;
+  /* position: absolute; */
+}
 
-  .slide-move {
-    transition: all .5s ease;
-  }
+.slide-enter {
+  transform: translateX(-2em);
+  opacity: 0;
+}
+
+.slide-leave-to {
+  transform: translateX(2em);
+  opacity: 0;
+}
+
+.slide-move {
+  transition: all 0.5s ease;
+}
 </style>
