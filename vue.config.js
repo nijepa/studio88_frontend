5b9a6e176ed498de2f3a0d12b298ea0902a9ccf8
Nikola Pavicevic <nijepa@gmail.com>
2021-04-09 12:25:50 +0200

    remove vue conf

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/studio88_frontend/' : '/',
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ? '/studio88_frontend/' : '/',
+// };
